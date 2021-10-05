b1ac0862dd1a693abe24d8ffc01e2caa9a3d6260
Reinert Yosua <reinert_yosua@yahoo.com>
2021-10-05 17:29:57 +0700

    Update point_of_sales_new.sql

--- point_of_sales_new.sql
+++ point_of_sales_new.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 22, 2021 at 06:53 PM
+-- Generation Time: Oct 05, 2021 at 12:28 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.10
 
@@ -40,15 +40,18 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Kabel', 'Kabel mobil', NULL, NULL),
-(2, 'Lampu', 'Lampu mobil', NULL, NULL),
-(3, 'KategoriA', 'tes kategori', '2021-09-22 08:53:12', '2021-09-22 08:53:12'),
-(4, 'kategori benar', 'kategori benar desc', '2021-09-22 08:56:55', '2021-09-22 09:48:24'),
-(5, 'uk', 'jk', '2021-09-22 08:59:07', '2021-09-22 08:59:07'),
-(6, 'hhh', 'kjlj', '2021-09-22 09:01:38', '2021-09-22 09:01:38'),
-(7, 'jjj', 'iohl', '2021-09-22 09:03:48', '2021-09-22 09:03:48'),
-(8, 'rarararara', 'wwwwwwwwwwwww', '2021-09-22 09:08:04', '2021-09-22 09:44:05'),
-(9, 'oooo', 'wwww', '2021-09-22 09:13:30', '2021-09-22 09:13:30');
+(1, 'aaa', 'aaa', '2021-10-04 21:10:41', '2021-10-04 21:11:33'),
+(2, 'bbb', 'bbb', '2021-10-04 21:10:48', '2021-10-04 21:11:27'),
+(3, 'ccc', 'ccc', '2021-10-04 21:10:56', '2021-10-04 21:11:20'),
+(4, 'ddd', 'ddd', '2021-10-04 21:11:03', '2021-10-04 21:11:12'),
+(5, 'eee', 'eee', '2021-10-04 21:11:46', '2021-10-04 21:11:46'),
+(6, 'fff', 'fff', '2021-10-04 21:11:54', '2021-10-04 21:11:54'),
+(7, 'ggg', 'ggg', '2021-10-04 21:12:02', '2021-10-04 21:12:02'),
+(8, 'hhh', 'hhh', '2021-10-04 21:12:12', '2021-10-04 21:12:12'),
+(9, 'iii', 'iii', '2021-10-04 21:12:20', '2021-10-04 21:12:20'),
+(10, 'jjj', 'jjj', '2021-10-04 21:12:32', '2021-10-04 21:12:32'),
+(11, 'kkk', 'kkk', '2021-10-04 21:12:41', '2021-10-04 21:12:41'),
+(12, 'lll', 'lll', '2021-10-04 21:12:51', '2021-10-04 21:12:51');
 
 -- --------------------------------------------------------
 
@@ -83,11 +86,13 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1),
-(4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2021_09_21_161051_create_category_table', 2);
+(8, '2014_10_12_000000_create_users_table', 1),
+(9, '2014_10_12_100000_create_password_resets_table', 1),
+(10, '2019_08_19_000000_create_failed_jobs_table', 1),
+(11, '2019_12_14_000001_create_personal_access_tokens_table', 1),
+(12, '2021_09_21_161051_create_category_table', 1),
+(14, '2021_10_02_061942_create_product_table', 1),
+(15, '2021_09_24_152543_create_supplier_table', 2);
 
 -- --------------------------------------------------------
 
@@ -122,6 +127,77 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `category_id` bigint(20) UNSIGNED NOT NULL,
+  `supplier_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `qty` int(11) NOT NULL,
+  `capital_price` int(11) NOT NULL,
+  `sell_price` int(11) NOT NULL,
+  `unit` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `category_id`, `supplier_id`, `name`, `type`, `qty`, `capital_price`, `sell_price`, `unit`, `description`, `created_at`, `updated_at`) VALUES
+(1, 3, 1, 'sdf', 'sdf', 1, 100000, 150000, 'pcs', 'tes', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `company_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `contact_title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `region` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `country` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `postal_code` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `account_number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bank_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `supplier`
+--
+
+INSERT INTO `supplier` (`id`, `company_name`, `contact_name`, `contact_title`, `address`, `city`, `region`, `country`, `postal_code`, `phone`, `account_number`, `bank_name`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'supa', 'aaa', 'aaa', 'aaa', 'aaa', 'aaa', 'aaa', '923', '0823', '0834', 'BCA', 'aaa', '2021-10-04 21:29:08', '2021-10-04 21:29:08'),
+(2, 'supb', 'bbb', 'bbb', 'bbb', 'bbb', 'bbb', 'bbb', '8323', '924', '2424', 'Mandiri', 'bbb', '2021-10-04 21:29:40', '2021-10-04 21:29:40'),
+(3, 'supc', 'ccc', 'ccc', 'ccc', 'ccc', 'ccc', 'ccc', '9224', '082424', '28424', 'BCA', 'ccc', '2021-10-04 21:30:04', '2021-10-04 21:30:04'),
+(4, 'supd', 'ddd', 'ddd', 'ddd', 'ddd', 'ddd', 'ddd', '224', '02424', '2424', 'BNI', 'ddd', '2021-10-04 21:30:37', '2021-10-04 21:30:37'),
+(5, 'supe', 'eee', 'eee', 'eee', 'eee', 'eee', 'eee', '114242', '08242', '028424', 'BCA', 'eee', '2021-10-04 21:31:04', '2021-10-04 21:31:04'),
+(6, 'supf', 'fff', 'fff', 'fff', 'fff', 'fff', 'fff', '923224', '0824', '28424', 'Mandiri', 'fff', '2021-10-04 21:31:39', '2021-10-04 21:31:39'),
+(7, 'supg', 'ggg', 'ggg', 'ggg', 'ggg', 'ggg', 'ggg', '29324', '024284', '2424', 'Mandiri', 'ggg', '2021-10-04 21:32:02', '2021-10-04 21:32:02'),
+(8, 'suph', 'hhh', 'hhh', 'hhh', 'hhh', 'hhh', 'hhh', '08224', '082424', '0824', 'BCA', 'hhh', '2021-10-04 21:32:24', '2021-10-04 21:32:24'),
+(9, 'supi', 'iii', 'iii', 'iii', 'iii', 'iii', 'iii', '9203424', '082411', '02424', 'BCA', 'iii', '2021-10-04 21:32:53', '2021-10-04 21:32:53'),
+(10, 'supj', 'jjj', 'jjj', 'jjj', 'jjj', 'jjj', 'jjj', '9242424', '082424', '824', 'BCA', 'jjj', '2021-10-04 21:33:33', '2021-10-04 21:33:33'),
+(11, 'supk', 'kkk', 'kkk', 'kkk', 'kkk', 'kkk', 'kkk', '082324', '89242', '92824', 'BNI', 'kkk', '2021-10-04 21:33:59', '2021-10-04 21:33:59'),
+(12, 'supl', 'lll', 'lll', 'lll', 'lll', 'lll', 'lll', '8242', '0823421', '082424', 'BNI', 'lll', '2021-10-04 21:34:21', '2021-10-04 21:34:21'),
+(13, 'supm', 'mmm', 'mmm', 'mmm', 'mmm', 'mmm', 'mmm', '8242', '082424', '872424', 'Mandiri', 'mmm', '2021-10-04 21:34:50', '2021-10-04 21:34:50');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -141,7 +217,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Reinert Yosua Rumagit', 'reinertyosuarumagit@gmail.com', NULL, '$2y$10$FB1YBlxVRT.QFI9mNoPZReKStXUC2X5brXi/ZisCI1MS8VWrjCj1.', NULL, '2021-09-21 09:02:41', '2021-09-21 09:02:41');
+(1, 'Reinert Yosua Rumagit', 'reinertyosuarumagit@gmail.com', NULL, '$2y$10$Qxy5.NPFOlPZQEMDyVr/pOF/HLoo9hmLrxlBBiMrSkYcAYFaJqpoK', NULL, '2021-10-05 02:55:05', '2021-10-05 02:55:05');
 
 --
 -- Indexes for dumped tables
@@ -181,6 +257,18 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -207,13 +295,25 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
