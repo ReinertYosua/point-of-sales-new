fe31582a6188612e90a76abbf3ed7939a2271050
Reinert Yosua <reinert_yosua@yahoo.com>
2021-10-20 12:10:21 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,6 +65,9 @@
                 <a class="navbar-brand" href="{{ url('/product') }}">
                     Product
                 </a>
+                <a class="navbar-brand" href="{{ url('/barangkeluar') }}">
+                    Barang Keluar
+                </a>
                 @endguest
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
